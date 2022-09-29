9cb2852d5defd337a09be7cdc8304fd86c3ea99c
muharramapri <75059964+muharramapri@users.noreply.github.com>
2022-09-29 20:48:08 +0700

    commit pertama

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos').then(async res => {
+      const resData = await res.json()
+
+      setTodos(resData)
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <ul>
+        {todos.map(item => {
+          return (
+            <li>
+              {item.userId}
+            </li>
+          )
+        })}
+      </ul>
+        <button type="sumbit"> Coba</button>
         <a
           className="App-link"
           href="https://reactjs.org"
